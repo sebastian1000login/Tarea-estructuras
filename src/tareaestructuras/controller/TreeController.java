17e259fb17e2194e80051176338789f6d13a5947
sebastian1000login <sebastian1000login@gmail.com>
2026-07-09 04:10:32 -0500

    Updated TreeController

--- src/tareaestructuras/controller/TreeController.java
+++ src/tareaestructuras/controller/TreeController.java
@@ -4,6 +4,7 @@
  */
 package tareaestructuras.controller;
 
+import tareaestructuras.model.Empleado;
 import tareaestructuras.model.Node;
 
 /**
@@ -18,5 +19,61 @@ public class TreeController
     {
     }
     
+    int height(Node node)
+    {
+        if (node == null)
+        {
+            return 0;
+        }else
+        {
+            return node.getHeight();
+        }
+    }
     
+    void rotateRight()
+    {
+        
+    }
+    
+    public void push(int id, String name, float salary)
+    {
+        Node aux = null;
+        Node back = null;
+        Empleado emp = new Empleado(id, name, salary);
+        Node nuevo = new Node(emp);
+        
+        if (root == null)
+        {
+            root = nuevo;
+        }else
+        {
+            aux = root;
+            while (aux != null)
+            {
+                back = aux;
+                if (id < aux.getEmp().getId())
+                {
+                    if (back.getRight() == null)
+                    {
+                        back.setHeight(back.getHeight() + 1);
+                    }
+                    aux = aux.getLeft();
+                }else
+                {
+                    if (back.getLeft() == null)
+                    {
+                        back.setHeight(back.getHeight() + 1);
+                    }
+                    aux = aux.getRight();
+                }
+            }
+            if (id < back.getEmp().getId())
+            {
+                back.setLeft(nuevo);
+            }else
+            {
+                back.setRight(nuevo);
+            }
+        }
+    }
 }
--- src/tareaestructuras/model/Node.java
+++ src/tareaestructuras/model/Node.java
@@ -11,13 +11,55 @@ package tareaestructuras.model;
 public class Node
 {
     Empleado emp;
+    int height;
     Node left;
     Node right;
 
     public Node(Empleado emp)
     {
         this.emp = emp;
+        this.height = 1;
         this.left = null;
         this.right = null;
+    }
+
+    public Empleado getEmp()
+    {
+        return emp;
+    }
+
+    public void setEmp(Empleado emp)
+    {
+        this.emp = emp;
+    }
+
+    public int getHeight()
+    {
+        return height;
+    }
+
+    public void setHeight(int height)
+    {
+        this.height = height;
+    }
+
+    public Node getLeft()
+    {
+        return left;
+    }
+
+    public void setLeft(Node left)
+    {
+        this.left = left;
+    }
+
+    public Node getRight()
+    {
+        return right;
+    }
+
+    public void setRight(Node right)
+    {
+        this.right = right;
     }
 }
