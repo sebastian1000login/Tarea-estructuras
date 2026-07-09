49d77af2b7834ef8dfbe4fa48ac65e27a983a3c5
sebastian1000login <sebastian1000login@gmail.com>
2026-07-08 22:07:22 -0500

    Added project folders

---
+++ src/tareaestructuras/TareaEstructuras.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tareaestructuras;
+
+/**
+ *
+ * @author sebastian
+ */
+public class TareaEstructuras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
