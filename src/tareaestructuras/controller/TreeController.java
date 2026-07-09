452f149c79427bbfa66ccf294d07c1c8ff62ee3b
sebastian1000login <sebastian1000login@gmail.com>
2026-07-08 22:22:01 -0500

    Fixed missing files

---
+++ src/tareaestructuras/controller/TreeController.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tareaestructuras.controller;
+
+import tareaestructuras.model.Node;
+
+/**
+ *
+ * @author sebastian
+ */
+public class TreeController
+{
+    Node root;
+
+    public TreeController()
+    {
+    }
+    
+    
+}
